0183db6e1259144898b6a6ab672498c0d3a11d20
Robert Reategui <robertdev18@gmail.com>
2017-08-17 16:23:03 -0500

    .

--- app/controllers/Web/Index.php
+++ app/controllers/Web/Index.php
@@ -31,9 +31,6 @@ class Index implements ControllerProviderInterface
     }
     public function index(Request $request, Application $app)
     {
-        //$x = $this->procedenciatxt(1,$app);
-        //echo "{$x}";
-        
         $jdcode = json_decode('[{"ContactPointId":"300000018988537","ContactPointType":"EMAIL","Status":"A","OwnerTableName":"HZ_PARTIES","OwnerTableId":"300000018984284","PrimaryFlag":"false","OrigSystemReference":"300000018988537","LastUpdateDate":"2017-07-26T22:22:09.038Z","LastUpdatedBy":"FORMWEB2","CreationDate":"2017-07-26T22:22:09.0Z","CreatedBy":"FORMWEB2","LastUpdateLogin":"5540C3F29CD516C0E053A79FC80AE011","RequestId":null,"ObjectVersionNumber":"1","CreatedByModule":"HZ_WS","ContactPointPurpose":"BUSINESS","PrimaryByPurpose":"N","StartDate":"2017-07-26","EndDate":"4712-12-31","RelationshipId":null,"PartyUsageCode":null,"OrigSystem":null,"EmailFormat":"","EmailAddress":"CESAR.HUASUPOMA.L@GMAIL.COM","PartyName":"CESAR HUASUPOMA","OverallPrimaryFlag":"false","EmailInformation":{"ContactPointId":"300000018988537","ContactPointType":"EMAIL","__FLEX_Context":null,"_FLEX_NumOfSegments":"0"}},{"ContactPointId":"300000019076601","ContactPointType":"EMAIL","Status":"A","OwnerTableName":"HZ_PARTIES","OwnerTableId":"300000018984284","PrimaryFlag":"false","OrigSystemReference":"300000019076601","LastUpdateDate":"2017-08-16T23:44:11.036Z","LastUpdatedBy":"FORMWEB2","CreationDate":"2017-08-16T23:44:11.001Z","CreatedBy":"FORMWEB2","LastUpdateLogin":"56E85C68A2EF378BE053A79FC80AB463","RequestId":null,"ObjectVersionNumber":"1","CreatedByModule":"HZ_WS","ContactPointPurpose":"BUSINESS","PrimaryByPurpose":"N","StartDate":"2017-08-16","EndDate":"4712-12-31","RelationshipId":null,"PartyUsageCode":null,"OrigSystem":null,"EmailFormat":"","EmailAddress":"diego.pachas2@ricoh-la.com","PartyName":"CESAR HUASUPOMA","OverallPrimaryFlag":"false","EmailInformation":{"ContactPointId":"300000019076601","ContactPointType":"EMAIL","__FLEX_Context":null,"_FLEX_NumOfSegments":"0"}},{"ContactPointId":"300000018988536","ContactPointType":"EMAIL","Status":"A","OwnerTableName":"HZ_PARTIES","OwnerTableId":"300000018984284","PrimaryFlag":"true","OrigSystemReference":"300000018988536","LastUpdateDate":"2017-08-17T16:58:13.112Z","LastUpdatedBy":"FORMWEB2","CreationDate":"2017-07-26T22:22:06.012Z","CreatedBy":"FORMWEB2","LastUpdateLogin":"56F6EAFC25E1964AE053A69FC80A49B3","RequestId":null,"ObjectVersionNumber":"7","CreatedByModule":"HZ_WS","ContactPointPurpose":"PERSONAL","PrimaryByPurpose":"N","StartDate":"2017-07-26","EndDate":"4712-12-31","RelationshipId":null,"PartyUsageCode":null,"OrigSystem":null,"EmailFormat":"","EmailAddress":"cesar.huasupoma@pucp.pe","PartyName":"CESAR HUASUPOMA","OverallPrimaryFlag":"true","EmailInformation":{"ContactPointId":"300000018988536","ContactPointType":"EMAIL","__FLEX_Context":null,"_FLEX_NumOfSegments":"0"}}]');
 
         $user = new \stdClass();
@@ -43,16 +40,16 @@ class Index implements ControllerProviderInterface
         $email2 = true;
         $email3 = true;
         foreach ($jdcode as $email) {
-            echo $email->EmailAddress.'-'.$email->Status.'-'.$email->PrimaryFlag .'<br>';
+            echo '<br>----'.$email->EmailAddress.'-'.$email->Status.'-'.$email->PrimaryFlag .'--------<br>';
             if (!empty($user->emailaddress2)) {
-                if ($user->emailaddress2 == $email->EmailAddress) {
+                if ($user->emailaddress2 === $email->EmailAddress) {
                     $email2 = false;
                 }
             } else {
                 $email2 = false;
             }
             if (!empty($user->persondeoctrcorreopucpc)) {
-                if ($user->persondeoctrcorreopucpc == $email->EmailAddress) {
+                if ($user->persondeoctrcorreopucpc === $email->EmailAddress) {
                     $email3 = false;
                 }
             } else {
@@ -60,10 +57,10 @@ class Index implements ControllerProviderInterface
             }
         }
         if ($email2 != false) {
-            echo $user->emailaddress2;
+            echo 'xml emailaddress2:'.$user->emailaddress2.'<br>';
         }
         if ($email3 != false) {
-            echo $user->emailaddress2;
+            echo 'xml pucp:'.$user->emailaddress2.'<br>';
         }
         return '';
     }
