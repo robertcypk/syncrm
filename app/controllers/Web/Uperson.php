190ceedfbca079e1f332a4d9d001f8d029c7f95e
Robert Reategui <robertdev18@gmail.com>
2017-08-17 16:52:35 -0500

    .

--- app/controllers/Web/Uperson.php
+++ app/controllers/Web/Uperson.php
@@ -1,98 +1,101 @@
 <?php
 namespace Web;
+
 use Web\Person;
 use Web\Soap;
 use Web\Emailuser;
 use Silex\Application;
-class Uperson{
-	var $wsPerson = 'https://cang-test.crm.us2.oraclecloud.com:443/foundationParties/PersonService?WSDL';
-	/* Update person */	
-	public function updatePerson($contacto, $dataForm){
-			$person = new Person();
-			$app = new Application();
-			$emails = $person->getPerson($contacto, 1);
-			
-			//$response['emails'] = json_encode($emails['Email']);
-			//return $response;
-			
-			if(!empty($emails)){
-				if(!empty($emails['Email'])){
-					$soap = new Soap;
-					$client = $soap->getClient($this->wsPerson);
-					$soapaction = "http://xmlns.oracle.com/apps/cdm/foundation/parties/personService/applicationModule/mergePerson";
-					$request = $this->updatePersonRequest($emails['Email'], $dataForm, $contacto);
-					
-					$savexml = new \Web\Logsrv();
-					$savexml->savelog($request,'Uperson');
-					//$savexml->savelog($emails['Email']['EmailAddress'],'Person-response');
-					
-					if(!empty($request)){
-						$response['response'] = $client->send($request, $soapaction, '');
-						$response['xml'] = base64_encode($request);
-						return $response;
-					}
-				}
-				
-			}
-			
-	}
 
-	public function updatePersonRequest($emails, $user, $contacto){
-		$email1 = true;
+class Uperson
+{
+    public $wsPerson = 'https://cang-test.crm.us2.oraclecloud.com:443/foundationParties/PersonService?WSDL';
+    /* Update person */
+    public function updatePerson($contacto, $dataForm)
+    {
+        $person = new Person();
+        $app = new Application();
+        $emails = $person->getPerson($contacto, 1);
+            
+        //$response['emails'] = json_encode($emails['Email']);
+        //return $response;
+            
+        if (!empty($emails)) {
+            if (!empty($emails['Email'])) {
+                $soap = new Soap;
+                $client = $soap->getClient($this->wsPerson);
+                $soapaction = "http://xmlns.oracle.com/apps/cdm/foundation/parties/personService/applicationModule/mergePerson";
+                $request = $this->updatePersonRequest($emails['Email'], $dataForm, $contacto);
+                    
+                $savexml = new \Web\Logsrv();
+                $savexml->savelog($request, 'Uperson');
+                //$savexml->savelog($emails['Email']['EmailAddress'],'Person-response');
+                    
+                if (!empty($request)) {
+                    $response['response'] = $client->send($request, $soapaction, '');
+                    $response['xml'] = base64_encode($request);
+                    return $response;
+                }
+            }
+        }
+    }
+
+    public function updatePersonRequest($emails, $user, $contacto)
+    {
+        $email1 = true;
         $email2 = true;
-		$email3 = true;
+        $email3 = true;
         $arraEmail = array();
-		//EmailAddress
-        if( empty($emails['EmailAddress']) ){
-        	$arraEmail = $emails;
-		}else{
-			$arraEmail = $emails['EmailAddress'];
-		}
+        //EmailAddress
+        if (count($emails['EmailAddress'])==1) {
+            $arraEmail = $emails['EmailAddress'];
+        } else {
+            $arraEmail = $emails;
+        }
 
-		$savexml = new \Web\Logsrv();
-		$savexml->savelog($arraEmail,'Person-updatePersonRequest');
+        $savexml = new \Web\Logsrv();
+        $savexml->savelog($arraEmail, 'Person-updatePersonRequest');
 
-		
-		foreach($arraEmail as $email){
-				/*if(!empty($user->emailaddress)){
-					if( $email['Status']=='A' and $email['PrimaryFlag']==true and (strtolower($user->emailaddress) == strtolower($email["EmailAddress"])) ){
-						$email1 = false;
-					}
-				}else{
-					$email1 = false;
-				}*/
-				if(!empty($user->emailaddress2)){
-					if($user->emailaddress2 === $email["EmailAddress"]){
-						$email2 = false;
-					}
-				}else{
-					$email2 = false;
-				}
-				if(!empty($user->persondeoctrcorreopucpc) ){
-					if($user->persondeoctrcorreopucpc === $email["EmailAddress"]){
-						$email3 = false;
-					}
-				}else{
-					$email3 = false;
-				}
+        
+        foreach ($arraEmail as $email) {
+            /*if(!empty($user->emailaddress)){
+                if( $email['Status']=='A' and $email['PrimaryFlag']==true and (strtolower($user->emailaddress) == strtolower($email["EmailAddress"])) ){
+                    $email1 = false;
+                }
+            }else{
+                $email1 = false;
+            }*/
+            if (!empty($user->emailaddress2)) {
+                if ($user->emailaddress2 === $email["EmailAddress"]) {
+                    $email2 = false;
+                }
+            } else {
+                $email2 = false;
+            }
+            if (!empty($user->persondeoctrcorreopucpc)) {
+                if ($user->persondeoctrcorreopucpc === $email["EmailAddress"]) {
+                    $email3 = false;
+                }
+            } else {
+                $email3 = false;
+            }
         }
 
         /*$request_xml_1 = '';
         if($email1==true){
-			$request_xml_1 ='<per:Email>
-					<con:OwnerTableName>HZ_PARTIES</con:OwnerTableName>
-					<con:OwnerTableId>'.$contacto['PartyId'].'</con:OwnerTableId>
-					<con:PrimaryFlag>true</con:PrimaryFlag>
-					<con:ContactPointPurpose>PERSONAL</con:ContactPointPurpose>
-					<con:EmailAddress>'.strtolower($user->emailaddress).'</con:EmailAddress>
-					<con:PrimaryByPurpose>N</con:PrimaryByPurpose>
-					<con:CreatedByModule>HZ_WS</con:CreatedByModule>
-				</per:Email>';
-		}*/
-		
-		$request_xml_2 = '';
-		if($email2==true){
-			$request_xml_2 ='<per:Email>
+            $request_xml_1 ='<per:Email>
+                    <con:OwnerTableName>HZ_PARTIES</con:OwnerTableName>
+                    <con:OwnerTableId>'.$contacto['PartyId'].'</con:OwnerTableId>
+                    <con:PrimaryFlag>true</con:PrimaryFlag>
+                    <con:ContactPointPurpose>PERSONAL</con:ContactPointPurpose>
+                    <con:EmailAddress>'.strtolower($user->emailaddress).'</con:EmailAddress>
+                    <con:PrimaryByPurpose>N</con:PrimaryByPurpose>
+                    <con:CreatedByModule>HZ_WS</con:CreatedByModule>
+                </per:Email>';
+        }*/
+        
+        $request_xml_2 = '';
+        if ($email2!=false) {
+            $request_xml_2 ='<per:Email>
 					<con:OwnerTableName>HZ_PARTIES</con:OwnerTableName>
 					<con:OwnerTableId>'.$contacto['PartyId'].'</con:OwnerTableId>
 					<con:PrimaryFlag>false</con:PrimaryFlag>
@@ -101,11 +104,11 @@ class Uperson{
 					<con:PrimaryByPurpose>N</con:PrimaryByPurpose>
 					<con:CreatedByModule>HZ_WS</con:CreatedByModule>
 				</per:Email>';
-		}
-		
-		$request_xml_3 = '';
-		if($email3==true){
-			$request_xml_3 ='<per:Email>
+        }
+        
+        $request_xml_3 = '';
+        if ($email3!=false) {
+            $request_xml_3 ='<per:Email>
 					<con:OwnerTableName>HZ_PARTIES</con:OwnerTableName>
 					<con:OwnerTableId>'.$contacto['PartyId'].'</con:OwnerTableId>
 					<con:PrimaryFlag>false</con:PrimaryFlag>
@@ -114,12 +117,12 @@ class Uperson{
 					<con:PrimaryByPurpose>N</con:PrimaryByPurpose>
 					<con:CreatedByModule>HZ_WS</con:CreatedByModule>
 				</per:Email>';
-		}
+        }
 
-		//'.$request_xml_1.'
+        //'.$request_xml_1.'
 
-		if($request_xml_2 || $request_xml_3){
-			$request_xml ='<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+        if ($request_xml_2 || $request_xml_3) {
+            $request_xml ='<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
 					xmlns:typ="http://xmlns.oracle.com/apps/cdm/foundation/parties/personService/applicationModule/types/"
 					xmlns:per="http://xmlns.oracle.com/apps/cdm/foundation/parties/personService/"
 					xmlns:par="http://xmlns.oracle.com/apps/cdm/foundation/parties/partyService/"
@@ -143,12 +146,9 @@ class Uperson{
 					</soapenv:Body>
 				</soapenv:Envelope>';
 
-				return $request_xml;
+            return $request_xml;
+        }
 
-		}
-
-		return '';
-	}
-	
+        return '';
+    }
 }
-?>
