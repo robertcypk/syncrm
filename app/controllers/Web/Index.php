55a5ff45a948c1fed56bba5d2f9ceb6be6cd029d
Robert Reategui <robertdev18@gmail.com>
2017-08-16 19:08:54 -0500

    convertir a mayusculas solo nombres y apellidos

--- app/controllers/Web/Index.php
+++ app/controllers/Web/Index.php
@@ -34,8 +34,9 @@ class Index implements ControllerProviderInterface{
 		$user = new \stdClass();
 		$user->emailaddress2 = 'cesar.huasupoma.l@gmail.com';
 		$user->persondeoctrcorreopucpc = 'cesar.huasupoma@pucp.pe';
-		print_r($jdcode);
+		//print_r($jdcode);
 		foreach($jdcode as $email){
+			echo $email->EmailAddress.'<br>';
 				if(!empty($user->emailaddress2)){
 					if( $email→Status=='A' and $email->PrimaryFlag ==false and 
 						$user->emailaddress2 == $email->EmailAddress ){
